932ada801751b66692dfe9fd53701d0b2047bab1
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2019-12-03 02:03:43 +0000

    Day 02 solutions.

---
+++ 02/solutions.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+
+from copy import deepcopy
+
+
+
+def process(data):
+    i=0
+    while i < len(data):
+        op = data[i]
+        a = data[i+1]
+        b = data[i+2]
+        c = data[i+3]
+        if op == 1:
+            data[c] = data[a] + data[b]
+        elif op == 2:
+            data[c] = data[a] * data[b]
+        elif op == 99:
+            return data[0], data[1], data[2]
+        else:
+            raise Exception('Invalid op code')
+        i += 4
+
+    return None
+
+
+
+
+
+if __name__ == '__main__':
+    with open('input', 'r') as f:
+        _data = list(map(int, f.readline().split(',')))
+
+    data = deepcopy(_data)
+    data[1] = 12
+    data[2] = 2
+    result, noun, verb = process(data)
+    print(result)
+
+    target = 19690720
+    for a in range(100):
+        data = deepcopy(_data)
+        data[1] = a
+        data[2] = 2
+        result, noun, verb = process(data)
+        if result > target:
+            a -= 1
+            break
+    for b in range(100):
+        data = deepcopy(_data)
+        data[1] = a
+        data[2] = b
+        result, noun, verb = process(data)
+        if result > target:
+            b -= 1
+            break
+
+    data = deepcopy(_data)
+    data[1] = 33
+    data[2] = 76
+    result, noun, verb = process(data)
+    answer = 100 * noun + verb
+    print(answer)
