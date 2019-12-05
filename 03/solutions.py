ee13b78199c37e38da8f8b04104715813b50ac72
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2019-12-05 00:45:17 +0000

    Solution day 03 part II.

--- 03/solutions.py
+++ 03/solutions.py
@@ -5,7 +5,10 @@ import numpy as np
 from collections import namedtuple
 
 
-def subparse(m, d):
+def parseSingleInstruction(m, d):
+    """
+    Parse a single instruction.
+    """
     if m == 'U':
         p2 = (0, d)
     elif m == 'D':
@@ -20,46 +23,29 @@ def subparse(m, d):
     return p2
 
 
-def parse(line):
+def parseInstructions(line):
+    """
+    Parse a all instructions.
+    """
     line = line.strip().split(',')
-    line = [ subparse(move[0], int(move[1:])) for move in line ]
+    line = [ parseSingleInstruction(move[0], int(move[1:])) for move in line ]
     return line
-
-
-class Axe:
-    def __init__(self):
-        self.position, self.min, self.max = 0, 0, 0
-
-    def __iadd__(self, a):
-        self.position += a
-        self.max = max(self.max, self.position)
-        return self
-
-    def __isub__(self, a):
-        self.position -= a
-        self.min = min(self.min, self.position)
-        return self
-
-    def __str__(self):
-        return f'{self.position}  [{self.min, self.max}]'
-
-
-class Position:
-    def __init__(self):
-        self.x = Axe()
-        self.y = Axe()
-
-    def __str__(self):
-        return f'x: {self.x}  y: {self.y}'
-
 
 
 Point = namedtuple('Point', ('x', 'y'))
 
 class Droite:
+    """
+    Holds two Points that define a Droite.
+    """
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
+
+
+    @property
+    def len(self):
+        return ManhattonDistance(self.p1, self.p2)
 
 
     @property
@@ -95,6 +81,9 @@ class Droite:
 
 
     def __or__(self, other):
+        """
+        Calculate the intersection between two Droites or return None if they don't intersect.
+        """
         if self.isVertical() and other.isVertical():
             return None
 
@@ -111,42 +100,65 @@ class Droite:
 
 
 
-def ManhattonDistance(point):
-    return sum(map(abs, point))
+def ManhattonDistance(p1, p2=Point(0, 0)):
+    """
+    Calculate the Manhattan distance between two points.
+    """
+    return abs(p1.x - p2.x) + abs(p1.y - p2.y)
 
 
 
 def minimumDistance(wire1, wire2):
+    wire1_segments = []
     p1 = Point(0, 0)
-    droites = []
     for x, y in wire1:
         p2 = Point(p1.x + x, p1.y + y)
-        droites.append(Droite(p1, p2))
+        wire1_segments.append(Droite(p1, p2))
         p1 = p2
 
-    print(droites, len(droites))
+    print(wire1_segments, len(wire1_segments))
 
-    minimums = []
+    minimums_distance = []  # Manhattan distances between origin and intersections
+    minimums_step = []  # Total wires' length when wires cross.
+
+    distance2 = 0  # wire 2's length
     p1 = Point(0, 0)
     for x, y in wire2:
         p2 = Point(p1.x + x, p1.y + y)
-        d = Droite(p1, p2)
+        d2 = Droite(p1, p2)
         p1 = p2
-        print(d)
+        print(d2)
 
-        for a in droites:
-            intersecting_point = d | a
+        distance1 = 0  # wire 1's length
+        for d1 in wire1_segments:
+            # Calculate intersection point.
+            intersecting_point = d2 | d1
+            # Are they intersecting?
             if intersecting_point != None:
                 distance = ManhattonDistance(intersecting_point)
-                print(d, a)
+                minimums_distance.append(distance)
+
+                steps = distance1 \
+                        + distance2 \
+                        + ManhattonDistance(intersecting_point, d1.p1) \
+                        + ManhattonDistance(intersecting_point, d2.p1)
+                minimums_step.append(steps)
+
+                print(d2, d1)
                 print(intersecting_point, distance)
-                minimums.append(distance)
+            distance1 += d1.len
 
-    print(minimums)
-    minimums.sort()
-    minimums = list(filter(lambda d: d!=0, minimums))
+        distance2 += d2.len
 
-    return minimums[0]
+    minimums_distance = list(filter(lambda d: d!=0, minimums_distance))
+    minimums_distance.sort()
+    print(minimums_distance)
+
+    minimums_step = list(filter(lambda d: d!=0, minimums_step))
+    minimums_step.sort()
+    print(minimums_step)
+
+    return minimums_distance[0], minimums_step[0]
 
 
 
@@ -155,18 +167,25 @@ def minimumDistance(wire1, wire2):
 if __name__ == '__main__':
     test = '''R75,D30,R83,U83,L12,D49,R71,U7,L72
               U62,R66,U55,R34,D71,R55,D58,R83'''
-    wires = list(map(lambda l: parse(l), test.splitlines()))
+    wires = list(map(lambda l: parseInstructions(l), test.splitlines()))
 
-    print(minimumDistance(*wires))
+    md, steps = minimumDistance(*wires)
+    assert md == 159
+    assert steps == 610
+
 
     test = '''R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
               U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'''
-    wires = list(map(lambda l: parse(l), test.splitlines()))
+    wires = list(map(lambda l: parseInstructions(l), test.splitlines()))
 
-    print(minimumDistance(*wires))
+    md, steps = minimumDistance(*wires)
+    assert md == 135
+    assert steps == 410
 
 
-    with open('input', 'r') as f:
-        wires = list(map(lambda l: parse(l), f.readlines()))
+    if True:
+        with open('input', 'r') as f:
+            wires = list(map(lambda l: parseInstructions(l), f.readlines()))
 
-    print(minimumDistance(*wires))
+        # Answers: 1337 & 65356
+        print(minimumDistance(*wires))
