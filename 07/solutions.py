b2d0356f5ff15a5c7059f15fe0e44bbc437bd706
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2019-12-07 14:42:20 +0000

    Backup.

--- 07/solutions.py
+++ 07/solutions.py
@@ -16,10 +16,25 @@ def amplify(pgm, settings, _input = 0):
 
 
 if __name__ == '__main__':
+    pgm = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+    amplify(pgm, (9,8,7,6,5))
+
     with open('input', 'r') as f:
         pgm = opcodeInt.parse_input(f.readline())
 
     answers = [ amplify(pgm, settings) for settings in tqdm(permutations(range(5), 5)) ]
+    answer = sorted(answers)[-1]
+
+    print(answers)
+    # Answer: 51679
+    print(answer)
+
+
+    _input = 0
+    answers = []
+    for settings in tqdm(permutations(range(5,10), 5)):
+        _input = amplify(pgm, settings, _input)
+        answers.append(_input)
     answer = sorted(answers)[-1]
 
     print(answers)
