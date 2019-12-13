50427cce03eb31918a62c23785cd90e994733e44
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2019-12-13 18:42:09 +0000

    Solution day 13 part I.

---
+++ 13/solutions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env  python3
+
+from collections import defaultdict
+
+from interpreter import Interpreter
+from parser import parse
+
+
+if __name__ == '__main__':
+    with open('input', 'r') as f:
+        pgm = parse(f.readline())
+
+    interpreter = Interpreter(pgm)
+
+    tiles = defaultdict(lambda: [])
+    grid = {}
+
+    while True:
+        x = interpreter()
+        if x is None:
+            break
+        y = interpreter()
+        tile_id = interpreter()
+        grid[(x,y)] = tile_id
+        tiles[tile_id].append((x,y))
+
+    # Answer: 213
+    print('Part I:', len(tiles[2]))
