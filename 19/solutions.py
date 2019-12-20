425cbae09969e5fe25d06cde91afa601ba92897c
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2019-12-20 01:39:14 +0000

    Solution day 19 part II.

--- 19/solutions.py
+++ 19/solutions.py
@@ -3,6 +3,7 @@
 import numpy as np
 
 from interpreter import Interpreter
+from itertools import count
 from parser import loadPgm
 
 
@@ -21,11 +22,36 @@ def partI(pgm):
     print(*grid, sep='\n')
     answer = sum(sum(line) for line in grid)
     print('PartI:', answer)
+    assert answer == 201
 
+
+
+def partII(pgm):
+    def check(x, y):
+        interpreter = Interpreter(pgm)
+        step = iter(interpreter)
+        interpreter.giveInputs((x,y))
+        return next(step)
+
+    x = 10
+    y = 150
+    while True:
+        for x in count(x):
+            if check(x, y) == 1:
+                break
+        print(f'x:{x}, y:{y}')
+        if check(x+99, y-99) == 1:
+            return x, y-99
+        y += 1
 
 
 
 
 if __name__ == '__main__':
     pgm = loadPgm()
+
     partI(pgm)
+
+    x, y = partII(pgm)
+    # BAD 6670993
+    print('PartII:', x*10000+y)
